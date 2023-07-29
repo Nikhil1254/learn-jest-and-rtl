8c17460e3ba7ccd2c2034abb472f6c0446ca83ec
Nikhil <np0129999@gmail.com>
2023-07-29 20:53:10 +0530

    Completed react-testing

--- src/__test__/skills.test.tsx
+++ src/__test__/skills.test.tsx
@@ -1,4 +1,4 @@
-import {  render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Skills from "../components/skills/Skills";
 
 describe("Skills Component", () => {
